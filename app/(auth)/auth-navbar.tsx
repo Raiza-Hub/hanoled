742bd2507919c730a7ec0acc9adf113b83472602
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-15 16:58:37 +0100

    fix: prevent navbar distortion on first load by specifying image dimensions

--- app/(auth)/auth-navbar.tsx
+++ app/(auth)/auth-navbar.tsx
@@ -5,11 +5,12 @@ const AuthNavbar = () => {
     return (
         <nav className="flex items-center justify-start p-2">
             <div className="ml-4 flex lg:ml-0">
-                <Link href="/" className="block w-[180px] h-[36px] relative">
+                <Link href="/" className="block w-[180px] relative aspect-[464/94]">
                     <Image
                         src="/hanoled_logo.jpg"
                         alt="Hanoled Logo"
-                        fill
+                        width={464}
+                        height={94}
                         priority
                         sizes="180px"
                         className="object-contain"
