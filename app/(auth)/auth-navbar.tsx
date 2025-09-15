4f458b337421a26f47a62128dada4f073e6794c4
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-15 16:08:36 +0100

    fix: removed height style from link and fill props from the image

--- app/(auth)/auth-navbar.tsx
+++ app/(auth)/auth-navbar.tsx
@@ -5,13 +5,13 @@ const AuthNavbar = () => {
     return (
         <nav className="flex items-center justify-start p-5 ">
             <div className="ml-4 flex lg:ml-0">
-                <Link href="/" className="block w-[180px] h-[60px] relative">
+                <Link href="/" className="block w-[180px] relative">
                     <Image
                         src="/hanoled_logo.jpg"
                         alt="Hanoled Logo"
-                        fill
                         priority
-                        sizes="180px"
+                        width={180}
+                        height={180}
                         className="object-contain"
                     />
                 </Link>
