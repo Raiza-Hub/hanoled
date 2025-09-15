febea38fbb1cee3f2142f8c9d1bbcad50334987d
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-15 14:44:38 +0100

    feat: add AuthNavbar with logo link to homepage

---
+++ app/(auth)/auth-navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const AuthNavbar = () => {
+    return (
+        <nav className="flex items-center justify-start p-5">
+            <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                    <Image
+                        src="/hanoled_logo.jpg"
+                        alt="Hanoled Logo"
+                        width={180}
+                        height={180}
+                        priority
+                        loading="eager"
+                    />
+                </Link>
+            </div>
+        </nav>
+    );
+};
+
+export default AuthNavbar;
