2e8cf02529f35a2e898da8c467fafaec350f5f04
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-15 14:25:31 +0100

    refactor: replace div with semantic <main> tag for layout container

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "@/components/auth-form/login-form";
 
 const Page = () => {
     return (
-        <div className="flex pt-20 flex-col items-center justify-center lg:px-0">
+        <main className="flex pt-20 flex-col items-center justify-center lg:px-0">
             <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:max-w-sm">
                 <div className="flex flex-col justify-center space-y-2">
                     <h1 className="text-3xl font-semibold tracking-tight">
@@ -38,7 +38,7 @@ const Page = () => {
                     </p>
                 </div>
             </div>
-        </div>
+        </main>
     );
 };
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import SignUpForm from "@/components/auth-form/signup-form";
 
 const Page = () => {
     return (
-        <div className="flex pt-20 flex-col items-center justify-center lg:px-0">
+        <main className="flex pt-20 flex-col items-center justify-center lg:px-0">
             <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:max-w-sm">
                 <div className="flex flex-col justify-center space-y-2">
                     <h1 className="text-3xl font-semibold tracking-tight">Get started</h1>
@@ -37,7 +37,7 @@ const Page = () => {
                     </p>
                 </div>
             </div>
-        </div>
+        </main>
     );
 };
 
