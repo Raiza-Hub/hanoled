bd133fddd952fc0fe8e44621f834819222e4cfd0
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-22 20:01:38 +0100

    faet: add environment validation with @t3-oss/env-nextjs and zod

---
+++ env.ts
@@ -0,0 +1,38 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    BETTER_AUTH_URL: z.string(),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_BASE_URL: z.url(),
+  },
+  /*
+   * Specify what values should be validated by your schemas above.
+   *
+   * If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+   * For Next.js >= 13.4.4, you can use the experimental__runtimeEnv option and
+   * only specify client-side variables.
+   */
+  runtimeEnv: {
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  // experimental__runtimeEnv: {
+  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  // }
+});
