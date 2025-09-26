37cee9ab18bf4b27e6c449c612fb17abb4bffaf1
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-26 01:11:31 +0100

    feat: add create school page with form component

---
+++ app/(main)/dashboard/create/page.tsx
@@ -0,0 +1,27 @@
+import SchoolForm from "@/components/school/create-school";
+import { Building2 } from "lucide-react";
+
+
+const Page = async () => {
+    return (
+        <div className="bg-background my-12">
+            <div className="mx-auto max-w-2xl bg-primary-foreground p-6 rounded-md border">
+                <div className="flex items-center gap-4 mb-6 ">
+                    <div className="hidden md:flex items-center p-2 border rounded-md ">
+                        <Building2 className="w-6 h-6" />
+                    </div>
+                    <div>
+                        <h1 className="font-semibold">Create a new school</h1>
+                        <p className="text-sm text-gray-500">
+                            Let’s get your account up and running.
+                        </p>
+                    </div>
+                </div>
+
+                <SchoolForm />
+            </div>
+        </div>
+    );
+};
+
+export default Page;
