b4928064cc51d486a8427aa04dc8c62f61a6f480
Raiza-Hub <wisdomadebola62@gmail.com>
2025-10-20 14:19:35 +0100

    feat: enhance form validation with strong passwords, OTP checks, and password confirmation

--- lib/validators/auth.ts
+++ lib/validators/auth.ts
@@ -4,7 +4,7 @@ export const SignIn = z.object({
   email: z.email({
     error: "Email is required",
   }),
-  password: z.string().min(1, {
+  password: z.string().min(8, {
     error: "Password is required",
   }),
 });
@@ -12,19 +12,63 @@ export const SignIn = z.object({
 export const SignUp = z.object({
   name: z
     .string()
-    .min(1, {
-      error: "Name is required.",
+    .min(1, { error: "Name is required." })
+    .max(50, { error: "Name must be at most 50 characters long." }),
+
+  email: z
+    .email({ error: "Email is required" }),
+
+  password: z
+    .string()
+    .min(8, { error: "Password is required" })
+    .refine((val) => /[0-9]/.test(val), {
+      error: "Password must contain at least one number.",
     })
-    .max(50, {
-      error: "Name must be at most 50 characters long.",
+    .refine((val) => /[a-z]/.test(val), {
+      error: "Password must contain at least one lowercase letter.",
+    })
+    .refine((val) => /[A-Z]/.test(val), {
+      error: "Password must contain at least one uppercase letter.",
     }),
-  email: z.email({
-    error: "Email is required",
-  }),
-  password: z.string().min(1, {
-    error: "Password is required",
-  }),
 });
+
+export const otpSchema = z
+  .string()
+  .regex(/^\d{7}$/, "OTP must be exactly 7 digits")
+
+export const forgetPassword = z.object({
+  email: z.email({ error: "Email is required" })
+})
+
+export const resetPassword = z
+  .object({
+    email: z.email({ error: "Email is required" }),
+
+    newPassword: z
+      .string()
+      .min(8, { error: "Password is required" })
+      .refine((val) => /[0-9]/.test(val), {
+        error: "Password must contain at least one number.",
+      })
+      .refine((val) => /[a-z]/.test(val), {
+        error: "Password must contain at least one lowercase letter.",
+      })
+      .refine((val) => /[A-Z]/.test(val), {
+        error: "Password must contain at least one uppercase letter.",
+      }),
+
+    confirmPassword: z.string(),
+
+    otp: z
+      .string()
+      .regex(/^\d{7}$/, "OTP must be exactly 7 digits")
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"], // attach the error to the confirmPassword field
+  });
 
 export type TSignIn = z.infer<typeof SignIn>;
 export type TSignUp = z.infer<typeof SignUp>;
+export type TForgetPassword = z.infer<typeof forgetPassword>
+export type TresetPassword = z.infer<typeof resetPassword>
