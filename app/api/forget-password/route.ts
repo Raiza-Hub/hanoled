4e33fa8fdcb3a3205a4e3fc6990799a4e2cedcf3
Raiza-Hub <wisdomadebola62@gmail.com>
2025-10-20 14:03:39 +0100

    feat: add forgot password route to handle email requests

---
+++ app/api/forget-password/route.ts
@@ -0,0 +1,33 @@
+import { env } from "@/env";
+import { ForgotPassword, GetNewOtpError, GetNewOtpSuccess } from "@/type";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { email } = await req.json();
+    const cookieHeader = req.headers.get("cookie") || "";
+      
+    const res = await fetch(`${env.SERVER_URL}/api/auth/forgotPassword`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Cookie": cookieHeader,
+        },
+      credentials: "include",
+      body: JSON.stringify({ email })
+    });
+
+    const data: ForgotPassword = await res.json();
+      
+    console.log(data);
+      
+
+    return NextResponse.json(data, { status: res.status });
+  } catch (err) {
+    console.error("Proxy error:", err);
+   const data: ForgotPassword = {
+      message: err instanceof Error ? err.message : "something went wrong."
+    };
+    return NextResponse.json(data, { status: 500 });
+  }
+}
