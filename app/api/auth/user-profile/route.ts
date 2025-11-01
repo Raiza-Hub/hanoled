829213147619f208e88564e5d8c58ab728f6641f
Raiza-Hub <wisdomadebola62@gmail.com>
2025-11-01 21:16:19 +0100

    feat: add user profile route to proxy request to backend auth service

---
+++ app/api/auth/user-profile/route.ts
@@ -0,0 +1,24 @@
+import { env } from "@/env";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const cookieHeader = req.headers.get("cookie") || "";
+
+    const res = await fetch(`${env.SERVER_URL}/api/auth/userProfile`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cookie": cookieHeader,
+      },
+      credentials: "include",
+    });
+
+    const data = await res.json();
+    
+    return NextResponse.json(data, { status: res.status });
+  } catch (error) {
+    console.error("Proxy error:", error);
+    return NextResponse.json({ error: "Failed to fetch all schools" }, { status: 500 });
+  }
+}
